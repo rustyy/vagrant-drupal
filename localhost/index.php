09d22f5ed6f894383327bdb9f2b0f6102293d5f8
Felix Hofmann <felix.hofmann@rustyy.de>
2015-08-06 17:33:03 +0200

    Adds readme file for htdocs directory and environment information into localhost

---
+++ localhost/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>Vagrant-Drupal Overview</title>
+</head>
+<body>
+
+<h1>Environment overview</h1>
+<ul>
+  <li>
+    <a href="/">Project folder</a>
+  </li>
+  <li>
+    <a href="info.php">PHP-Info</a>
+  </li>
+  <li>
+    <a href="/phpmyadmin">phpMyAdmin</a>
+  </li>
+</ul>
+
+</body>
+</html>
+
+
+
+
